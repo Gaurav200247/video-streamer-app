17c576b15bdc3e61da1783e8390f27e915e9f8cb
Gaurav Gupta <gauravgupta742002@gmail.com>
2023-07-28 01:25:28 +0530

    server deployed on render

--- client/src/Actions/BASE_URL.js
+++ client/src/Actions/BASE_URL.js
@@ -2,4 +2,6 @@
 
 // export const BASE_URL = "http://localhost:4000";
 
-export const BASE_URL = "https://video-streamer-app.vercel.app";
+// export const BASE_URL = "https://video-streamer-app.vercel.app";
+
+export const BASE_URL = "https://video-streamer-app-server.onrender.com/";
