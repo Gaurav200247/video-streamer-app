97aa758dc00b88685b4d5a0148b2d794e59c8df8
Gaurav Gupta <gauravgupta742002@gmail.com>
2023-07-29 00:38:32 +0530

    back to vercel deployment

--- client/src/Actions/BASE_URL.js
+++ client/src/Actions/BASE_URL.js
@@ -2,6 +2,6 @@
 
 // export const BASE_URL = "http://localhost:4000";
 
-// export const BASE_URL = "https://video-streamer-app.vercel.app";
+export const BASE_URL = "https://video-streamer-app.vercel.app";
 
-export const BASE_URL = "https://video-streamer-app-server.onrender.com";
+// export const BASE_URL = "https://video-streamer-app-server.onrender.com";
