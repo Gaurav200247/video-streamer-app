6947c3c319e91402501ade58b4840d88dab311b8
Gaurav Gupta <gauravgupta742002@gmail.com>
2023-08-30 15:46:44 +0530

    changed logout cookie maxAge 2

--- server/Controllers/UserControllers/All_Users/Logout.js
+++ server/Controllers/UserControllers/All_Users/Logout.js
@@ -2,16 +2,15 @@ const { StatusCodes } = require("http-status-codes");
 
 //-------User LogOut-------
 const LogOut = async (req, res) => {
-  res.cookie("userToken", null, {
-    maxAge: new Date(Date.now() + 1000),
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    path: "/",
-  });
-
   res
     .status(StatusCodes.OK)
+    .cookie("userToken", null, {
+      expires: new Date(Date.now() + 1000),
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+    })
     .json({ success: true, msg: "Logged Out Successfully !!" });
 };
 
